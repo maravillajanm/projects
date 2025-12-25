eba432f7bbdcb8e9ed927d6a37105f0d2159aebb
JanMaravilla - MRM <78269027+maravillajanm@users.noreply.github.com>
2025-12-25 11:30:44 +0800

    Mern Test Client Side

--- client/src/components/GenInfo.jsx
+++ client/src/components/GenInfo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import HomepageProducts from "./HomepageProducts";
 
 const GenInfo = () => {
   const data = [
@@ -51,7 +51,6 @@ const GenInfo = () => {
 export default GenInfo;
 
 export const Brands = () => {
-  const navigate = useNavigate();
 
   const data = [
     { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
@@ -63,30 +62,7 @@ export const Brands = () => {
   return (
     <div className="flex flex-col items-center my-16 w-full">
       <p className="prose prose-2xl font-bold mb-6">Top Brands</p>
-      <div className="flex flex-wrap justify-center">
-        {data.map((elem, id) => (
-          <div
-            key={id}
-            className="relative w-[340px] h-[340px] mx-2 mb-6 hover:text-white"
-          >
-            <div className="absolute w-full flex justify-center items-center top-4  ">
-              <p className="logo font-semibold z-50 ">{elem.name}</p>
-            </div>
-            <img
-              src={elem.src}
-              alt={elem.name}
-              className="w-full h-full object-cover"
-            />
-            <button
-              onClick={() => navigate(elem.to)}
-              className="absolute inset-0 flex items-center justify-center
-                             bg-gray-800 text-white opacity-0 hover:opacity-80 transition-opacity duration-200"
-            >
-              Explore →
-            </button>
-          </div>
-        ))}
-      </div>
+      <HomepageProducts data={data} />
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use('/api/products', router);
+
 const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
